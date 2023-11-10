04231d78163674ab0da0c0ab24a783596524b8e8
Kausik MR <kausikkratos@gmail.com>
2023-11-10 21:35:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# actions_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/actions_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.actions_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
